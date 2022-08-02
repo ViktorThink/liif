56870c89ba629bb2244a3c5f633f89514aef207d
ViktorThink <viktor.bowallius@hotmail.com>
2022-08-02 11:50:59 +0200

    Added config

---
+++ liif/inference_pytorch.py
@@ -0,0 +1,78 @@
+import argparse
+import os
+from PIL import Image
+
+import torch
+from torchvision import transforms
+
+from utils import make_coord
+
+
+def make(model_spec, args=None, load_sd=True):
+    
+    model_spec = torch.load(args.model)['model']
+    if args is not None:
+        model_args = copy.deepcopy(model_spec['args'])
+        model_args.update(args)
+    else:
+        model_args = model_spec['args']
+    model = models[model_spec['name']](**model_args)
+    if load_sd:
+        model.load_state_dict(model_spec['sd'])
+    return model.cuda()
+
+
+def getArgs()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', default='input.png')
+    parser.add_argument('--model')
+    parser.add_argument('--resolution')
+    parser.add_argument('--output', default='output.png')
+    parser.add_argument('--gpu', default='0')
+    args = parser.parse_args()
+    return args
+
+
+
+def prepareInput(image_path, resolution):
+    img = transforms.ToTensor()(Image.open(image_path).convert('RGB'))
+    h, w = list(map(int, resolution.split(',')))
+    coord = make_coord((h, w)).cuda()
+    cell = torch.ones_like(coord)
+    cell[:, 0] *= 2 / h
+    cell[:, 1] *= 2 / w
+    
+    return img, h, w, coord, cell
+
+def batched_predict(model, inp, coord, cell, bsize):
+    with torch.no_grad():
+        model.gen_feat(inp)
+        n = coord.shape[1]
+        ql = 0
+        preds = []
+        while ql < n:
+            qr = min(ql + bsize, n)
+            pred = model.query_rgb(coord[:, ql: qr, :], cell[:, ql: qr, :])
+            preds.append(pred)
+            ql = qr
+        pred = torch.cat(preds, dim=1)
+    return pred
+
+
+if __name__ == '__main__':
+    arge = getArgs()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    img = transforms.ToTensor()(Image.open(args.input).convert('RGB'))
+
+    model = models.make(args.model) 
+    
+    img, h, w, coord, cell = prepareInput(args.input, args.resolution)
+
+
+    pred = batched_predict(model, ((img - 0.5) / 0.5).cuda().unsqueeze(0),
+        coord.unsqueeze(0), cell.unsqueeze(0), bsize=30000)[0]
+    pred = (pred * 0.5 + 0.5).clamp(0, 1).view(h, w, 3).permute(2, 0, 1).cpu()
+    
+    transforms.ToPILImage()(pred).save(args.output)
