cfd54a6f8e23e83318e05cb919a2604133723752
ViktorThink <viktor.bowallius@hotmail.com>
2022-08-02 12:10:05 +0200

    Added config

--- liif/demo.py
+++ liif/demo.py
@@ -5,7 +5,7 @@ from PIL import Image
 import torch
 from torchvision import transforms
 
-import models.models
+from models import models
 from utils import make_coord
 from test import batched_predict
 
